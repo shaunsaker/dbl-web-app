137eb768cc0acf5a7e8a1d798b5afa9029e48b0b
Shaun Saker <sakershaun@gmail.com>
2022-05-21 10:13:31 +0200

    wip

--- src/server/services/firebase/index.ts
+++ src/server/services/firebase/index.ts
@@ -3,8 +3,10 @@ import { ServiceAccount } from 'firebase-admin'
 
 import serviceAccount from './service-account.json'
 
-firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount as ServiceAccount),
-})
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    credential: firebase.credential.cert(serviceAccount as ServiceAccount),
+  })
+}
 
 export { firebase }
